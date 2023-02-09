1641698459c9c4b9a452ec527c21c9b59a695131
@EkaHvo <ekasagalaeva@gmail.com>
2023-02-09 18:34:50 +0300

    init

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className='header'>
+      <img className='icon' src='https://i.pinimg.com/236x/8a/de/fe/8adefe5af862b4f9cec286c6ee4722cb.jpg'></img>
+    </header>
+  );
+}
+
+export default Header;
